3be248a6b12893b242a2c7c23bf64c2be4cff653
tridentweb <webdesigntrident@gmail.com>
2024-06-22 14:09:42 +0900

    追記

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,9 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  const state = e.target.checked;
+  if(state){
+    document.getElementById('text').style.backgroundColor = "red";
+  }else{
+    document.getElementById('text').style.backgroundColor = "transparent";
+  }
 }
